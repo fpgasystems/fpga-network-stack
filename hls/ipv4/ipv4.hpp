0e2b661c00e82008c48f129ad5113c076fa81674
David Sidler <david.sidler@inf.ethz.ch>
2019-01-12 16:02:58 +0100

    moved IPv4 checksum calculation

--- hls/ipv4/ipv4.hpp
+++ hls/ipv4/ipv4.hpp
@@ -44,6 +44,295 @@ struct ipv4Meta
 			:their_address(addr(127,96)), length(len) {}
 };
 
+template <int N>
+struct subSums
+{
+	ap_uint<17>		sum[N];
+};
+
+
+void compute_ipv4_checksum(	hls::stream<net_axis<64> >&	dataIn,
+							hls::stream<net_axis<64> >&	dataOut,
+							hls::stream<subSums<4> >&	subSumFiFoOut,
+							const bool					skipChecksum=false)
+{
+	#pragma HLS PIPELINE II=1
+	#pragma HLS INLINE off
+
+	static subSums<4>	cics_ip_sums;
+	static ap_uint<4> cics_ipHeaderLen;
+	static ap_uint<3> cics_wordCount = 0;
+
+	ap_uint<16> temp;
+
+	if (!dataIn.empty())
+	{
+		net_axis<64> currWord = dataIn.read();
+		dataOut.write(currWord);
+
+		switch (cics_wordCount)
+		{
+		case 0:
+			cics_ipHeaderLen = currWord.data.range(3, 0);
+
+			for (int i = 0; i < 4; i++)
+			{
+				#pragma HLS unroll
+				temp(7, 0) = currWord.data.range(i*16+15, i*16+8);
+				temp(15, 8) = currWord.data.range(i*16+7, i*16);
+				cics_ip_sums.sum[i] += temp;
+				cics_ip_sums.sum[i] = (cics_ip_sums.sum[i] + (cics_ip_sums.sum[i] >> 16)) & 0xFFFF;
+			}
+			cics_ipHeaderLen -= 2; 
+			cics_wordCount++;
+			break;
+		case 1:
+			for (int i = 0; i < 4; i++)
+			{
+				#pragma HLS unroll
+				temp(7, 0) = currWord.data.range(i*16+15, i*16+8);
+				temp(15, 8) = currWord.data.range(i*16+7, i*16);
+				if (!skipChecksum || i != 1)
+				{
+					cics_ip_sums.sum[i] += temp;
+					cics_ip_sums.sum[i] = (cics_ip_sums.sum[i] + (cics_ip_sums.sum[i] >> 16)) & 0xFFFF;
+				}
+			}
+			cics_ipHeaderLen -= 2; 
+			cics_wordCount++;
+			break;
+		default:
+			switch (cics_ipHeaderLen)
+			{
+			case 0:
+				//length 0 means we are just handling payload
+				break;
+			case 1:
+				// Sum up part 0-1
+				for (int i = 0; i < 2; i++)
+				{
+					#pragma HLS unroll
+					temp(7, 0) = currWord.data.range(i*16+15, i*16+8);
+					temp(15, 8) = currWord.data.range(i*16+7, i*16);
+					cics_ip_sums.sum[i] += temp;
+					cics_ip_sums.sum[i] = (cics_ip_sums.sum[i] + (cics_ip_sums.sum[i] >> 16)) & 0xFFFF;
+				}
+				cics_ipHeaderLen = 0;
+				subSumFiFoOut.write(subSums<4>(cics_ip_sums));
+				break;
+			default:
+				// Sum up everything
+				for (int i = 0; i < 4; i++)
+				{
+				#pragma HLS unroll
+					temp(7, 0) = currWord.data.range(i*16+15, i*16+8);
+					temp(15, 8) = currWord.data.range(i*16+7, i*16);
+					cics_ip_sums.sum[i] += temp;
+					cics_ip_sums.sum[i] = (cics_ip_sums.sum[i] + (cics_ip_sums.sum[i] >> 16)) & 0xFFFF;
+				}
+				if (cics_ipHeaderLen == 2)
+				{
+					subSumFiFoOut.write(subSums<4>(cics_ip_sums));
+				}
+				cics_ipHeaderLen -= 2;
+				break;
+			} // switch ipHeaderLen
+			break;
+		} // switch WORD_N
+		if (currWord.last)
+		{
+			for (int i = 0; i < 4; i++)
+			{
+				#pragma HLS unroll
+				cics_ip_sums.sum[i] = 0;
+			}
+			cics_wordCount = 0;
+		}
+	}
+}
+
+void compute_ipv4_checksum(	hls::stream<net_axis<512> >&	dataIn,
+							hls::stream<net_axis<512> >&	dataOut,
+							hls::stream<subSums<32> >&		subSumFiFoOut,
+							const bool						skipChecksum=false)
+{
+	#pragma HLS PIPELINE II=1
+	#pragma HLS INLINE off
+
+	static subSums<32>	cics_ip_sums;
+	static ap_uint<4> cics_ipHeaderLen;
+	static bool cics_firstWord = true;
+
+	ap_uint<16> temp;
+
+	if (!dataIn.empty())
+	{
+		net_axis<512> currWord = dataIn.read();
+		dataOut.write(currWord);
+
+		if (cics_firstWord)
+		{
+			cics_ipHeaderLen = currWord.data(3, 0);
+
+			for (int i = 0; i < 32; i++)
+			{
+				#pragma HLS unroll
+				temp = reverse((ap_uint<16>) currWord.data(i*16+15, i*16));
+				if (!skipChecksum || i != 5)
+				{
+					if ((i/2) < cics_ipHeaderLen)
+					{
+						cics_ip_sums.sum[i] += temp;
+						cics_ip_sums.sum[i] = (cics_ip_sums.sum[i] + (cics_ip_sums.sum[i] >> 16)) & 0xFFFF;
+					}
+				}
+			}
+			subSumFiFoOut.write(subSums<32>(cics_ip_sums));
+			cics_firstWord = false;
+		}
+
+		if (currWord.last)
+		{
+			for (int i = 0; i < 32; i++)
+			{
+				#pragma HLS unroll
+				cics_ip_sums.sum[i] = 0;
+			}
+			cics_firstWord = true;
+		}
+	}
+}
+
+template <int N>
+void check_ipv4_checksum(	hls::stream<subSums<N> >&	subSumFiFoIn,
+							hls::stream<bool>&			checkSumFifoOut)
+{
+	#pragma HLS PIPELINE II=1
+	#pragma HLS INLINE off
+
+	static ap_uint<1> state = 0;
+	static subSums<N> icic_ip_sums;
+
+	switch (state)
+	{
+	case 0:
+		if (!subSumFiFoIn.empty())
+		{
+			icic_ip_sums = subSumFiFoIn.read();
+
+			if (N >= 32)
+			{
+				for (int i = 0; i < 16; i++)
+				{
+					#pragma HLS unroll
+					icic_ip_sums.sum[i] += icic_ip_sums.sum[i+16];
+					icic_ip_sums.sum[i] = (icic_ip_sums.sum[i] + (icic_ip_sums.sum[i] >> 16)) & 0xFFFF;
+				}
+			}
+
+			if (N >= 16)
+			{
+				for (int i = 0; i < 8; i++)
+				{
+					#pragma HLS unroll
+					icic_ip_sums.sum[i] += icic_ip_sums.sum[i+8];
+					icic_ip_sums.sum[i] = (icic_ip_sums.sum[i] + (icic_ip_sums.sum[i] >> 16)) & 0xFFFF;
+				}
+			}
+
+			if (N >= 8)
+			{
+				for (int i = 0; i < 4; i++)
+				{
+					#pragma HLS unroll
+					icic_ip_sums.sum[i] += icic_ip_sums.sum[i+4];
+					icic_ip_sums.sum[i] = (icic_ip_sums.sum[i] + (icic_ip_sums.sum[i] >> 16)) & 0xFFFF;
+				}
+			}
+			state = 1;
+		}
+		break;
+	case 1:
+		icic_ip_sums.sum[0] += icic_ip_sums.sum[2];
+		icic_ip_sums.sum[1] += icic_ip_sums.sum[3];
+		icic_ip_sums.sum[0] = (icic_ip_sums.sum[0] + (icic_ip_sums.sum[0] >> 16)) & 0xFFFF;
+		icic_ip_sums.sum[1] = (icic_ip_sums.sum[1] + (icic_ip_sums.sum[1] >> 16)) & 0xFFFF;
+		icic_ip_sums.sum[0] += icic_ip_sums.sum[1];
+		icic_ip_sums.sum[0] = (icic_ip_sums.sum[0] + (icic_ip_sums.sum[0] >> 16)) & 0xFFFF;
+		icic_ip_sums.sum[0] = ~icic_ip_sums.sum[0];
+		std::cout << "checksum: " << std::hex << (uint16_t) icic_ip_sums.sum[0](15, 0) << std::endl;
+		checkSumFifoOut.write(icic_ip_sums.sum[0](15, 0) == 0);
+		state = 0;
+		break;
+	} //switch
+
+}
+
+template <int N>
+void finalize_ipv4_checksum(hls::stream<subSums<N> >&	subSumFiFoIn,
+							hls::stream<ap_uint<16> >&	checkSumFifoOut)
+{
+	#pragma HLS PIPELINE II=1
+	#pragma HLS INLINE off
+
+	static ap_uint<1> state = 0;
+	static subSums<N> icic_ip_sums;
+
+	switch (state)
+	{
+	case 0:
+		if (!subSumFiFoIn.empty())
+		{
+			icic_ip_sums = subSumFiFoIn.read();
+
+			if (N >= 32)
+			{
+				for (int i = 0; i < 16; i++)
+				{
+					#pragma HLS unroll
+					icic_ip_sums.sum[i] += icic_ip_sums.sum[i+16];
+					icic_ip_sums.sum[i] = (icic_ip_sums.sum[i] + (icic_ip_sums.sum[i] >> 16)) & 0xFFFF;
+				}
+			}
+
+			if (N >= 16)
+			{
+				for (int i = 0; i < 8; i++)
+				{
+					#pragma HLS unroll
+					icic_ip_sums.sum[i] += icic_ip_sums.sum[i+8];
+					icic_ip_sums.sum[i] = (icic_ip_sums.sum[i] + (icic_ip_sums.sum[i] >> 16)) & 0xFFFF;
+				}
+			}
+
+			if (N >= 8)
+			{
+				for (int i = 0; i < 4; i++)
+				{
+					#pragma HLS unroll
+					icic_ip_sums.sum[i] += icic_ip_sums.sum[i+4];
+					icic_ip_sums.sum[i] = (icic_ip_sums.sum[i] + (icic_ip_sums.sum[i] >> 16)) & 0xFFFF;
+				}
+			}
+			state = 1;
+		}
+		break;
+	case 1:
+		icic_ip_sums.sum[0] += icic_ip_sums.sum[2];
+		icic_ip_sums.sum[1] += icic_ip_sums.sum[3];
+		icic_ip_sums.sum[0] = (icic_ip_sums.sum[0] + (icic_ip_sums.sum[0] >> 16)) & 0xFFFF;
+		icic_ip_sums.sum[1] = (icic_ip_sums.sum[1] + (icic_ip_sums.sum[1] >> 16)) & 0xFFFF;
+		icic_ip_sums.sum[0] += icic_ip_sums.sum[1];
+		icic_ip_sums.sum[0] = (icic_ip_sums.sum[0] + (icic_ip_sums.sum[0] >> 16)) & 0xFFFF;
+		icic_ip_sums.sum[0] = ~icic_ip_sums.sum[0];
+		std::cout << "checksum: " << std::hex << (uint16_t) icic_ip_sums.sum[0](15, 0) << std::endl;
+		checkSumFifoOut.write(icic_ip_sums.sum[0](15, 0));
+		state = 0;
+		break;
+	} //switch
+
+}
+
 /**
  * [7:4] = version;
  * [3:0] = IHL;
@@ -109,12 +398,12 @@ public:
 	}
 };
 
-void ipv4(		hls::stream<axiWord>&	s_axis_rx_data,
-				hls::stream<ipv4Meta>&		m_axis_rx_meta,
-				hls::stream<axiWord>&	m_axis_rx_data,
-				hls::stream<ipv4Meta>&		s_axis_tx_meta,
-				hls::stream<axiWord>&	s_axis_tx_data,
-				hls::stream<axiWord>&	m_axis_tx_data,
-				ap_uint<32>			local_ipv4_address);
+void ipv4(	hls::stream<axiWord>&	s_axis_rx_data,
+			hls::stream<ipv4Meta>&	m_axis_rx_meta,
+			hls::stream<axiWord>&	m_axis_rx_data,
+			hls::stream<ipv4Meta>&	s_axis_tx_meta,
+			hls::stream<axiWord>&	s_axis_tx_data,
+			hls::stream<axiWord>&	m_axis_tx_data,
+			ap_uint<32>			local_ipv4_address);
 
 #endif
